9741131fa644ae02157e972c118680ca94e0e379
Hapalo Halyna <hapalohalia@gmail.com>
2025-06-28 15:14:44 +0300

    feat(DART-06): all task completed

--- 1_task.dart
+++ 1_task.dart
@@ -1,5 +1,6 @@
 void main() {
-  // Завдання 1: Математичні оператори
+  print('Завдання 1: Математичні оператори');
+  print('');
   double width = 18.4;
   double height = 30.9;
   double area = width * height;
@@ -8,28 +9,36 @@ void main() {
   print('Площа: $area');
   print('Периметр: $perimeter');
   print('Повних квадратів 1x1: $squares');
-
-  
-  // Завдання 2: Оператори порівняння
+  print('----------------');
+  print('');
+  print('Завдання 2: Оператори порівняння');
+  print('');
   int number = 75;
   print('Більше 50: ${number > 50}');
   print('Менше 100: ${number < 100}');
   print('Ділиться на 5: ${number % 5 == 0}');
-
-
-// Завдання 3: Логічні оператори
+  print('----------------');
+  print('');
+  print('Завдання 3: Логічні оператори');
+  print('');
   bool hasMoney = true;
   bool isStoreOpen = false;
   print('Можна купити: ${hasMoney && isStoreOpen}');
   print('Почекати: ${!hasMoney || !isStoreOpen}');
-
-    int temperature = 7;
+  print('----------------');
+  print('');
+  print('Завдання 4: Тернарний оператор');
+  print('');
+  int temperature = 7;
   String weather = temperature > 25 ? "Тепло" : "Прохолодно";
   String extra = temperature < 10 ? "Дуже холодно" : "";
   print('Погода: $weather');
   print(extra);
-
-    int score = 0;
+  print('----------------');
+  print('');
+  print('Завдання 5: Оператори присвоєння');
+  print('');
+  int score = 0;
   int correctAnswers = 17;
   int mistakes = 3;
   int totalQuestions = 20;
@@ -38,8 +47,10 @@ void main() {
   score *= 2;
   score ~/= totalQuestions;
   print('Результат: $score');
-
-    // Завдання 6: if/else
+  print('----------------');
+  print('');
+  print('Завдання 6: if/else');
+  print('');
   int examScore = 56;
   if (examScore > 90) {
     print("Відмінно");
@@ -53,7 +64,10 @@ void main() {
       print("Повторити курс");
     }
   }
-   // Завдання 7: switch
+  print('----------------');
+  print('');
+  print('Завдання 7: switch');
+  print('');
   int dayNumber = 6;
   switch (dayNumber) {
     case 1:
@@ -80,8 +94,10 @@ void main() {
     default:
       print("Невірний номер дня");
   }
-
-    // Завдання 8: for
+  print('----------------');
+  print('');
+  print('Завдання 8: for');
+  print('');
   int sum = 0;
   print("Числа 1–10:");
   for (int i = 1; i <= 10; i++) {
@@ -90,8 +106,10 @@ void main() {
     sum += i;
   }
   print("Сума: $sum");
-
-    // Завдання 9: while
+  print('----------------');
+  print('');
+  print('Завдання 9: while');
+  print('');
   int count = 10;
   while (count >= 0) {
     if (count % 2 == 0) {
@@ -101,7 +119,12 @@ void main() {
     }
     count--;
   }
-    // Завдання 10: break/continue
+  print('----------------');
+  print('');
+  print('Завдання 10: break/continue');
+  print('');
+
+  // Завдання 10: break/continue
   for (int i = 1; i <= 20; i++) {
     if (i % 3 == 0) continue;
     if (i > 15) break;
