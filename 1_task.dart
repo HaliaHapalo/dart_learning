ec566f4326a3a0d5eb8d14f9848d30e5efcb94fa
Hapalo Halyna <hapalohalia@gmail.com>
2025-06-28 14:53:26 +0300

    feat(DART-06): task 5 completed

--- 1_task.dart
+++ 1_task.dart
@@ -28,4 +28,14 @@ void main() {
   String extra = temperature < 10 ? "Дуже холодно" : "";
   print('Погода: $weather');
   print(extra);
+
+    int score = 0;
+  int correctAnswers = 17;
+  int mistakes = 3;
+  int totalQuestions = 20;
+  score += correctAnswers * 10;
+  score -= mistakes * 5;
+  score *= 2;
+  score ~/= totalQuestions;
+  print('Результат: $score');
 }
